b5546089ee7ca64b3ed2af5c0980d02ba12a2d29
Gabriel <gabrielveiga54@gmail.com>
2024-01-15 13:24:28 -0300

    first commit

---
+++ aula05/HelloWorld.java
@@ -0,0 +1,9 @@
+package aula05;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		//javac complica o programa
+		System.out.println("Hello World!");
+	
+	}
+}
---
+++ aula06/Argumentos.java
@@ -0,0 +1,10 @@
+package aula06;
+
+public class Argumentos {
+
+	public static void main(String[] args) {
+		//recebe argumentos do cmd
+			System.out.println("Olá mundo" + args[0]);
+		
+	}
+}
