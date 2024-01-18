b5842acbf446be011e580000d5953580fd461c62
Gabriel <gabrielveiga54@gmail.com>
2024-01-18 17:43:59 -0300

    some adjustments in class aula20

--- com/veiga/cursojava/aula20/Matrizes.java
+++ com/veiga/cursojava/aula20/Matrizes.java
@@ -1,5 +1,20 @@
 package com.veiga.cursojava.aula20;
 
 public class Matrizes {
-
+	
+	public static void main(String[] args) {
+		
+		double[][] notasAlunos = new double[30][4];
+		
+		for (int i = 0; i < notasAlunos.length; i++) {
+			int soma = 0;
+			for (int j = 0; j < notasAlunos[i].length; j++) {
+				System.out.println(notasAlunos[i][j]);
+			}
+			System.out.println();
+		}
+		
+		
+		
+	}
 }
